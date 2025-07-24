bfe397248688f6010e514d2d98ed18f806f8a08b
Ckilic123 <kilicccem@gmail.com>
2025-07-24 16:40:56 +0200

    Add files via upload

--- api/stocks.js
+++ api/stocks.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   const results = [];
 
   for (const symbol of symbols) {
-    const response = await fetch(\`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=\${symbol}&apikey=7G0SE75620MX4XO3\`);
+    const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=7G0SE75620MX4XO3`);
     const data = await response.json();
     results.push(data["Global Quote"]);
   }
