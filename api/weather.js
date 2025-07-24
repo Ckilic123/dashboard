7d51df05f0573470f45084a5edf53dfb29787308
Ckilic123 <kilicccem@gmail.com>
2025-07-24 16:55:25 +0200

    Delete api directory

--- api/weather.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch("https://api.weatherapi.com/v1/current.json?key=145f1fe14a0544cd9ec133705252407&q=Munich");
-  const data = await response.json();
-  res.status(200).json(data);
-}
